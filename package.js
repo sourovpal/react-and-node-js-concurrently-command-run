b30608f655f4d8ad4aea562f0a71a56f5666b246
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-01-18 03:46:56 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,16 @@
+"scripts": {
+    "start": "react-scripts start ",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject",
+    "dev": "concurrently \"npm run start\" \"cd ./server && npm run start\""
+  },
+  
+  
+  
+  //server site
+  
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon index.js"
+  },
